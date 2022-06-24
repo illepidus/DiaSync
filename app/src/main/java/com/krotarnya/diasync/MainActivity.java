8208d56f47f9a8baa61e0cf631953e707e47338e
Ivan <illepidus@gmail.com>
2022-06-24 16:36:18 +0300

    Stylish icon and some skeleton staff for test purposes

--- app/src/main/java/com/krotarnya/diasync/MainActivity.java
+++ app/src/main/java/com/krotarnya/diasync/MainActivity.java
@@ -3,12 +3,24 @@ package com.krotarnya.diasync;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private Button btn_detonate;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btn_detonate = (Button) findViewById( R.id.btn_detonate );
+        btn_detonate.setOnClickListener( this );
+    }
+
+    public void onClick(View view) {
+        if(view == btn_detonate) {
+            Log.d("DETONATED: ", "Detonation");
+        }
     }
 }
