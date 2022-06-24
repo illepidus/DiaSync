1f724a9806724efab0fb69f1454d8c020aa8e541
Ivan <illepidus@gmail.com>
2022-06-24 15:33:50 +0300

    Initial commit

---
+++ app/src/main/java/com/krotarnya/diasync/DiaBroadcastReceiver.java
@@ -0,0 +1,14 @@
+package com.krotarnya.diasync;
+
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+
+public class DiaBroadcastReceiver extends android.content.BroadcastReceiver {
+    private static final String TAG = "DiaBroadcastReceiver";
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        //Just log
+        Log.d(TAG, intent.getAction());
+    }
+}
