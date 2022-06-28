7a84b2c75ec6d8fcf61960299b9a5fda2c386b90
Ivan <illepidus@gmail.com>
2022-06-28 16:24:01 +0300

    refactor DiaBroadcastReceiver

--- app/src/main/java/com/krotarnya/diasync/DiaBroadcastReceiver.java
+++ app/src/main/java/com/krotarnya/diasync/DiaBroadcastReceiver.java
@@ -16,68 +16,66 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class DiaBroadcastReceiver extends android.content.BroadcastReceiver {
-    private static final String TAG = "DiaBroadcastReceiver";
+    private static final String TAG      = "DiaBroadcastReceiver";
+    private static final String ds_token = "78e297d6d6a5fe57da3184705551a69a4";
+    private static final String ds_url    = "https://krotarnya.ru/diasync.php";
+    private Context broadcast_context;
 
     @Override
     public void onReceive(Context context, Intent intent) {
-        final String url = "https://krotarnya.ru/diasync.php";
-        final String token = "78e297d6d6a5fe57da3184705551a69a4";
         final String action = intent.getAction();
         final Bundle bundle = intent.getExtras();
-
-        String update;
-        String type;
-
-        GsonBuilder builder = new GsonBuilder();
-        builder.registerTypeAdapterFactory(new BundleTypeAdapterFactory());
-        Gson gson = builder.create();
+        broadcast_context = context;
 
         if (action == null) return;
 
-        Log.d (TAG, "Received broadcast intent [" + action + "] with following extras: ");
+        Log.d (TAG, "Received broadcast intent [" + action + "] in context [" + context.toString() + "] with following extras: ");
         for (String key: bundle.keySet()) {
             Log.d (TAG, "\"" + key + "\" => [" + bundle.get(key).getClass().getSimpleName() + "]");
         }
 
-        switch (action) {
-            case "com.eveningoutpost.dexdrip.diasync.libre2_activation":
-                if (!bundle.containsKey("sensor") || !bundle.containsKey("bleManager")) {
-                    Log.e(TAG,"Received faulty libre2_activation intent");
-                    return;
-                }
-                update = gson.toJson(bundle);
-                type = "libre2_activation";
-                break;
-            case "com.eveningoutpost.dexdrip.diasync.libre2_bg":
-                if (!bundle.containsKey("glucose") || !bundle.containsKey("timestamp") || !bundle.containsKey("bleManager")) {
-                    Log.e(TAG,"Received faulty libre2_bg intent");
-                    return;
-                }
-                update = gson.toJson(bundle);
-                type = "libre2_bg";
-                break;
-            case "com.eveningoutpost.dexdrip.diasync.l2r":
-                //REMOVE THIS LATER
-                update = gson.toJson(bundle);
-                type = "REMOVE_THIS_TYPE_OF_UPDATES";
-                break;
-            default:
-                Log.e(TAG, "Unknown action: " + action);
+        if (action.equals("com.eveningoutpost.dexdrip.diasync.libre2_activation")) {
+            if (!bundle.containsKey("sensor") || !bundle.containsKey("bleManager")) {
+                Log.e(TAG,"Received faulty libre2_activation intent");
                 return;
+            }
+            sendUpdate(bundle, "libre2_activation");
+            return;
         }
 
-        RequestQueue request_queue = Volley.newRequestQueue(context);
-        StringRequest string_request = new StringRequest(Request.Method.POST, url, response -> Log.d(TAG, "Response: " + response), error -> Log.e(TAG, error.toString())) {
+        if (action.equals("com.eveningoutpost.dexdrip.diasync.libre2_bg")) {
+            if (!bundle.containsKey("glucose") || !bundle.containsKey("timestamp") || !bundle.containsKey("bleManager")) {
+                Log.e(TAG, "Received faulty libre2_bg intent");
+                return;
+            }
+            sendUpdate(bundle, "libre2_bg");
+        }
+        if (action.equals("com.eveningoutpost.dexdrip.diasync.libre2_bg_follower")) {
+            return;
+        }
+
+        Log.e(TAG,"Received unknown intent");
+    }
+
+    private void sendUpdate(Bundle bundle, String type) {
+        GsonBuilder builder = new GsonBuilder();
+        builder.registerTypeAdapterFactory(new BundleTypeAdapterFactory());
+        Gson gson = builder.create();
+        sendUpdate(gson.toJson(bundle), type);
+    }
+
+    private void sendUpdate(String update, String type) {
+        RequestQueue request_queue = Volley.newRequestQueue(broadcast_context);
+        StringRequest string_request = new StringRequest(Request.Method.POST, ds_url, response -> Log.d(TAG, "Response: " + response), error -> Log.e(TAG, error.toString())) {
             @Override
             protected Map<String,String> getParams(){
                 Map<String,String> params = new HashMap<>();
-                params.put("token", token);
+                params.put("token",  ds_token);
                 params.put("update", update);
-                params.put("type", type);
+                params.put("type",   type);
                 return params;
             }
         };
-
         request_queue.add(string_request);
     }
 }
