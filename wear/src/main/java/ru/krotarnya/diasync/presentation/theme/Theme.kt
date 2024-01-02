44d71fa82b92970b1732ca0b200b357051475787
ivblinov <ivblinov@yandex-team.ru>
2024-01-02 12:09:50 +0300

    Initial

---
+++ wear/src/main/java/ru/krotarnya/diasync/presentation/theme/Theme.kt
@@ -0,0 +1,17 @@
+package ru.krotarnya.diasync.presentation.theme
+
+import androidx.compose.runtime.Composable
+import androidx.wear.compose.material.MaterialTheme
+
+@Composable
+fun DiaSyncTheme(
+    content: @Composable () -> Unit
+) {
+    /**
+     * Empty theme to customize for your app.
+     * See: https://developer.android.com/jetpack/compose/designsystems/custom
+     */
+    MaterialTheme(
+        content = content
+    )
+}
